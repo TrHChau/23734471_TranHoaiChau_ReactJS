7e45ae179ad47b0c7cea5c628f71883347eb897b
Chau <trhoaichau11@gmail.com>
2026-01-19 14:38:46 +0700

    ...

---
+++ my-react-app/src/app.jsx
@@ -0,0 +1,13 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <Button type="primary">Primary</Button>
+      <Button type="danger">Danger</Button>
+      <Button type="success">Success</Button>
+    </div>
+  );
+}
+
+export default App;
